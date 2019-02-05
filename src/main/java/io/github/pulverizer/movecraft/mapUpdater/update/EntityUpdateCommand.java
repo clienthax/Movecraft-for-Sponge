a87d9c3b3df05cf6c8a2f8bf25180d009d133aea
BernardisGood <Pulverizer@users.noreply.github.com>
2019-02-05 17:01:06 +0000

    Revert "hmm"
    This reverts commit a13b091354d1d1483bae18a1ca5fb82f911f57a3.

--- src/main/java/io/github/pulverizer/movecraft/mapUpdater/update/EntityUpdateCommand.java
+++ src/main/java/io/github/pulverizer/movecraft/mapUpdater/update/EntityUpdateCommand.java
@@ -4,12 +4,11 @@ import com.flowpowered.math.vector.Vector3d;
 import io.github.pulverizer.movecraft.Movecraft;
 import io.github.pulverizer.movecraft.config.Settings;
 import org.spongepowered.api.entity.Entity;
-import org.spongepowered.api.world.Location;
 
 import java.util.Objects;
 
 /**
- * Class that stores the data about a single entity change to the map in an unspecified world. The world is retrieved contextually from the submitting craft.
+ * Class that stores the data about a single blocks changes to the map in an unspecified world. The world is retrieved contextually from the submitting craft.
  */
 public class EntityUpdateCommand extends UpdateCommand {
     private final Entity entity;
@@ -31,21 +30,7 @@ public class EntityUpdateCommand extends UpdateCommand {
         if (Settings.Debug)
             Movecraft.getInstance().getLogger().info("Attempting to move entity of type: " + entity.getType().getName());
 
-        boolean entityMoved = entity.setLocationAndRotation(new Location<>(entity.getWorld(), newLocation), entity.getRotation().add(0, yaw, 0));
-
-        if (entityMoved) {
-            if (Settings.Debug)
-                Movecraft.getInstance().getLogger().info("Moved Entity of type: " + entity.getType().getName());
-
-            return true;
-
-        } else {
-            if (Settings.Debug)
-                Movecraft.getInstance().getLogger().info("Failed to move Entity of type: " + entity.getType().getName());
-
-            return false;
-
-        }
+        return Movecraft.getInstance().getWorldHandler().moveEntity(entity, newLocation, yaw);
     }
 
     @Override
