38669cd1945b3a165b929ad7e9c576d70cb47278
BernardisGood <Pulverizer@users.noreply.github.com>
2019-04-14 22:04:28 +0100

    Hopefully fixed #7

--- src/main/java/io/github/pulverizer/movecraft/listener/BlockListener.java
+++ src/main/java/io/github/pulverizer/movecraft/listener/BlockListener.java
@@ -1,31 +1,23 @@
 package io.github.pulverizer.movecraft.listener;
 
-import io.github.pulverizer.movecraft.Movecraft;
+import com.google.common.collect.ImmutableSet;
 import io.github.pulverizer.movecraft.utils.MathUtils;
 import io.github.pulverizer.movecraft.MovecraftLocation;
 import io.github.pulverizer.movecraft.craft.Craft;
 import io.github.pulverizer.movecraft.config.Settings;
 import io.github.pulverizer.movecraft.craft.CraftManager;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
-import org.spongepowered.api.block.BlockState;
 import org.spongepowered.api.block.BlockType;
 import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.data.Transaction;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.entity.Entity;
-import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.entity.Item;
+import org.spongepowered.api.entity.explosive.PrimedTNT;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.projectile.explosive.fireball.SmallFireball;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
-import org.spongepowered.api.event.data.ChangeDataHolderEvent;
 import org.spongepowered.api.event.entity.SpawnEntityEvent;
-import org.spongepowered.api.event.filter.cause.Root;
-import org.spongepowered.api.event.filter.type.Include;
-import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.text.Text;
-import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
 import java.util.*;
@@ -35,36 +27,52 @@ import static org.spongepowered.api.event.Order.LAST;
 
 public class BlockListener {
 
-    final BlockType[] fragileBlocks = new BlockType[]{BlockTypes.BED, BlockTypes.PISTON_HEAD, BlockTypes.PISTON_EXTENSION, BlockTypes.TORCH, BlockTypes.REDSTONE_WIRE, BlockTypes.STANDING_SIGN, BlockTypes.WOODEN_DOOR, BlockTypes.LADDER, BlockTypes.WALL_SIGN, BlockTypes.LEVER, BlockTypes.STONE_PRESSURE_PLATE, BlockTypes.IRON_DOOR, BlockTypes.WOODEN_PRESSURE_PLATE, BlockTypes.UNLIT_REDSTONE_TORCH, BlockTypes.REDSTONE_TORCH, BlockTypes.STONE_BUTTON, BlockTypes.TRAPDOOR, BlockTypes.TRIPWIRE_HOOK, BlockTypes.TRIPWIRE, BlockTypes.WOODEN_BUTTON, BlockTypes.LIGHT_WEIGHTED_PRESSURE_PLATE, BlockTypes.HEAVY_WEIGHTED_PRESSURE_PLATE, BlockTypes.DAYLIGHT_DETECTOR, BlockTypes.DAYLIGHT_DETECTOR_INVERTED, BlockTypes.CARPET, BlockTypes.UNPOWERED_REPEATER, BlockTypes.POWERED_REPEATER, BlockTypes.UNPOWERED_COMPARATOR, BlockTypes.UNPOWERED_COMPARATOR};
     private long lastDamagesUpdate = 0;
 
     @Listener(order = LAST)
-    public void onBlockBreak(final ChangeBlockEvent.Break event, @Root Player player) {
+    public void onBlockBreak(final ChangeBlockEvent.Break event) {
 
-        List<Transaction<BlockSnapshot>> blocks = event.getTransactions();
-        for (Transaction<BlockSnapshot> transaction : blocks) {
-            BlockSnapshot block = transaction.getOriginal();
-            if (Settings.ProtectPilotedCrafts) {
-                MovecraftLocation mloc = MathUtils.sponge2MovecraftLoc(block.getLocation().get());
-                World blockWorld = block.getLocation().get().getExtent();
-                for (Craft craft : CraftManager.getInstance().getCraftsInWorld(blockWorld)) {
-                    if (craft == null || craft.getDisabled()) {
-                        continue;
-                    }
-                    for (MovecraftLocation tloc : craft.getHitBox()) {
-                        if (tloc.equals(mloc)) {
-                            player.sendMessage(Text.of("BLOCK IS PART OF A PILOTED CRAFT"));
-                            transaction.setValid(false);
+        if (event.getCause().root() instanceof PrimedTNT || event.getCause().root() instanceof SmallFireball)
+            return;
+
+        for (Transaction<BlockSnapshot> transaction : event.getTransactions()) {
+            BlockSnapshot blockSnapshot = transaction.getOriginal();
+
+            if (event.getCause().root() instanceof Player) {
+
+                Player player = (Player) event.getCause().root();
+
+                if (Settings.ProtectPilotedCrafts) {
+                    MovecraftLocation mloc = MathUtils.sponge2MovecraftLoc(blockSnapshot.getLocation().get());
+                    World blockWorld = blockSnapshot.getLocation().get().getExtent();
+                    for (Craft craft : CraftManager.getInstance().getCraftsInWorld(blockWorld)) {
+                        if (craft == null) {
+                            continue;
+                        }
+                        for (MovecraftLocation tloc : craft.getHitBox()) {
+                            if (tloc.equals(mloc)) {
+                                player.sendMessage(Text.of("BLOCK IS PART OF A PILOTED CRAFT"));
+                                transaction.setValid(false);
+                            }
                         }
                     }
                 }
+
+            } else {
+
+                for (Craft craft : CraftManager.getInstance().getCraftsInWorld(blockSnapshot.getLocation().get().getExtent())) {
+                    MovecraftLocation mLoc = MathUtils.sponge2MovecraftLoc(blockSnapshot.getLocation().get());
+                    if (!MathUtils.locIsNearCraftFast(craft, mLoc))
+                        transaction.setValid(false);
+                }
+
             }
         }
     }
 
     // prevent items from dropping from moving crafts
     @Listener(order = FIRST)
-    public void onItemSpawn(final SpawnEntityEvent event) {
+    public void onItemSpawn(SpawnEntityEvent event) {
 
         event.filterEntities(entity -> {
 
@@ -110,30 +118,7 @@ public class BlockListener {
         }
     }
 
-    /* TODO: RE-ADD NEXT!!!!
-    Keys.POWER.registerEvent(BlockState.class, event -> {});
-    // process certain redstone on cruising crafts
-    @Listener(order = FIRST)
-    public void onRedstoneEvent(ChangeDataHolderEvent.ValueChange event) {
-        if (event.getTargetHolder() == Keys.POWER)
-            return;
-
-        BlockSnapshot block = event.getBlock();
-        CraftManager.getInstance().getCraftsInWorld(block.getWorld());
-        for (Craft tcraft : CraftManager.getInstance().getCraftsInWorld(block.getWorld())) {
-            MovecraftLocation mloc = new MovecraftLocation(block.getX(), block.getY(), block.getZ());
-            if (MathUtils.locIsNearCraftFast(tcraft, mloc) &&
-                    tcraft.getCruising() &&
-                    (block.getTypeId() == 29 ||
-                    block.getTypeId() == 33 ||
-                    block.getTypeId() == 23 &&
-                    !tcraft.isNotProcessing())) {
-                event.setNewCurrent(event.getOldCurrent()); // don't allow piston movement on cruising crafts
-                return;
-            }
-        }
-    }
-
+    /*
     // prevent pistons on cruising crafts
     @Listener(order = FIRST)
     public void onPistonEvent(BlockPistonExtendEvent event) {
@@ -165,43 +150,9 @@ public class BlockListener {
         }
     }
 
-    // prevent fragile items from dropping on cruising crafts
-    @Listener(order = FIRST)
-    public void onPhysics(BlockPhysicsEvent event) {
-        if (event.isCancelled()) {
-            return;
-        }
-
-        Block block = event.getBlock();
-
-        final int[] fragileBlocks = new int[]{26, 34, 50, 55, 63, 64, 65, 68, 69, 70, 71, 72, 75, 76, 77, 93, 94, 96, 131, 132, 143, 147, 148, 149, 150, 151, 171, 193, 194, 195, 196, 197};
-        CraftManager.getInstance().getCraftsInWorld(block.getWorld());
-        for (Craft tcraft : CraftManager.getInstance().getCraftsInWorld(block.getWorld())) {
-            MovecraftLocation mloc = new MovecraftLocation(block.getX(), block.getY(), block.getZ());
-            if (!MathUtils.locIsNearCraftFast(tcraft, mloc)) {
-                continue;
-            }
-            if (Arrays.binarySearch(fragileBlocks, block.getTypeId()) >= 0) {
-                MaterialData m = block.getState().getData();
-                BlockFace face = BlockFace.DOWN;
-                boolean faceAlwaysDown = false;
-                if (block.getTypeId() == 149 || block.getTypeId() == 150 || block.getTypeId() == 93 || block.getTypeId() == 94)
-                    faceAlwaysDown = true;
-                if (m instanceof Attachable && !faceAlwaysDown) {
-                    face = ((Attachable) m).getAttachedFace();
-                }
-                if (!event.getBlock().getRelative(face).getType().isSolid()) {
-//						if(event.getEventName().equals("BlockPhysicsEvent")) {
-                    event.setCancelled(true);
-                    return;
-                }
-            }
-        }
-    }
-
     @Listener(order = LAST)
     public void onBlockIgnite(BlockIgniteEvent event) {
-        // replace blocks with fire occasionally, to prevent fast craft from simply ignoring fire
+        // replace blocks with fire occasionally, to prevent fast crafts from simply ignoring fire
         if (!Settings.FireballPenetration ||
                 event.isCancelled() ||
                 event.getCause() != BlockIgniteEvent.IgniteCause.FIREBALL) {
@@ -228,7 +179,7 @@ public class BlockListener {
         for (Craft craft : CraftManager.getInstance().getCraftsInWorld(e.getBlock().getWorld())) {
             if (craft != null &&
                     !craft.isNotProcessing() &&
-                    MathUtils.locIsNearCraftFast(craft, MathUtils.bukkit2MovecraftLoc(e.getBlock().getLocation()))) {
+                    MathUtils.locIsNearCraftFast(craft, MathUtils.sponge2MovecraftLoc(e.getBlock().getLocation()))) {
                 e.setCancelled(true);
                 return;
             }
